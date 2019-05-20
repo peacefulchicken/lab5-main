b8624a9b291ba636c11921b1101839c1292c6ce2
peacefulchicken <49442107+peacefulchicken@users.noreply.github.com>
2019-05-20 23:11:39 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/android_lab/MainActivity.java
+++ app/src/main/java/com/example/android_lab/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android_lab;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,28 +9,30 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MySQLite db;
     private ArrayList<String> target;
-    private ArrayAdapter adapter;
+    private SimpleCursorAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        String[] values = new String[] { "Pies", "Kot", "Koń","Gołąb", "Kruk", "Dzik", "Karp", "Osioł", "Chomik", "Mysz", "Jeż", "Kraluch" };
+        String[] values = new String[]{"Pies", "Kot", "Koń", "Gołąb", "Kruk", "Dzik", "Karp", "Osioł", "Chomik", "Mysz", "Jeż", "Kraluch"};
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.db = new MySQLite(this);
         this.target = new ArrayList<String>();
         this.target.addAll(Arrays.asList(values));
 
-        this.adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,this.target);
-
-        ListView listview = (ListView) findViewById( R.id.listView );
+        this.adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, db.lista(), new String[]{"_id", "gatunek"}, new int[]{android.R.id.text1, android.R.id.text2}, SimpleCursorAdapter.IGNORE_ITEM_VIEW_TYPE);
+        ListView listview = (ListView) findViewById(R.id.listView);
         listview.setAdapter(this.adapter);
     }
 
@@ -37,20 +40,22 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu, menu)
-        ;return true;
+        ;
+        return true;
     }
 
-    public void nowyWpis(MenuItem mi){
+    public void nowyWpis(MenuItem mi) {
         Intent intencja = new Intent(this, dodajWpis.class);
         startActivityForResult(intencja, 1);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode==1 && resultCode==RESULT_OK)   {Bundle extras = data.getExtras();
-        String nowy = (String)extras.get("wpis");
-        target.add(nowy);
-        adapter.notifyDataSetChanged();
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            String nowy = (String) extras.get("wpis");
+            target.add(nowy);
+            adapter.notifyDataSetChanged();
         }
     }
 }
