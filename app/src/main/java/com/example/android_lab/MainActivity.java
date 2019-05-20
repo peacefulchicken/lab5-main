289c6d4875ffc501d425033721309edcdbf6693b
peacefulchicken <49442107+peacefulchicken@users.noreply.github.com>
2019-05-20 19:10:30 +0200

    Lab4Start

---
+++ app/src/main/java/com/example/android_lab/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.android_lab;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> target;
+    private ArrayAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        String[] values = new String[] { "Pies", "Kot", "Koń","Gołąb", "Kruk", "Dzik", "Karp", "Osioł", "Chomik", "Mysz", "Jeż", "Kraluch" };
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.target = new ArrayList<String>();
+        this.target.addAll(Arrays.asList(values));
+
+        this.adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,this.target);
+
+        ListView listview = (ListView) findViewById( R.id.listView );
+        listview.setAdapter(this.adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu)
+        ;return true;
+    }
+
+    public void nowyWpis(MenuItem mi){
+        Intent intencja = new Intent(this, dodajWpis.class);
+        startActivityForResult(intencja, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==1 && resultCode==RESULT_OK)   {Bundle extras = data.getExtras();
+        String nowy = (String)extras.get("wpis");
+        target.add(nowy);
+        adapter.notifyDataSetChanged();
+        }
+    }
+}
