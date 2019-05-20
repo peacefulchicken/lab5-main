ed8687e78dbf06cfb766e42458d7781f83582d43
peacefulchicken <49442107+peacefulchicken@users.noreply.github.com>
2019-05-21 01:13:27 +0200

    Finalcommit
    Finalpush

--- app/src/main/java/com/example/android_lab/MainActivity.java
+++ app/src/main/java/com/example/android_lab/MainActivity.java
@@ -26,43 +26,47 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        String[] values = new String[]{"Pies", "Kot", "Koń", "Gołąb", "Kruk", "Dzik", "Karp", "Osioł", "Chomik", "Mysz", "Jeż", "Kraluch"};
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         this.db = new MySQLite(this);
-        this.target = new ArrayList<String>();
-        this.target.addAll(Arrays.asList(values));
 
         this.adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, db.lista(), new String[]{"_id", "gatunek"}, new int[]{android.R.id.text1, android.R.id.text2}, SimpleCursorAdapter.IGNORE_ITEM_VIEW_TYPE);
         ListView listview = (ListView) findViewById(R.id.listView);
         listview.setAdapter(this.adapter);
 
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapter, View view, int pos, long id)
-            {
+            public void onItemClick(AdapterView<?> adapter, View view, int pos, long id) {
+                Intent intencja = new Intent(getApplicationContext(), DodajWpis.class);
                 TextView name = (TextView) view.findViewById(android.R.id.text1);
                 Animal zwierz = db.pobierz(Integer.parseInt(name.getText().toString()));
-                Intent intencja = new Intent(getApplicationContext(), dodajWpis.class);intencja.putExtra("element", zwierz);startActivityForResult(intencja, 2);
+                intencja.putExtra("element", zwierz);
+                startActivityForResult(intencja, 1);
 
             }
         });
-
+        listview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView name = (TextView) view.findViewById(android.R.id.text1);
+                db.usun(name.getText().toString());
+                adapter.changeCursor(db.lista());
+                adapter.notifyDataSetChanged();
+                return true;
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu)
-        ;
+        inflater.inflate(R.menu.main_menu, menu);
         return true;
     }
 
     public void nowyWpis(MenuItem mi) {
-        Intent intencja = new Intent(this, dodajWpis.class);
+        Intent intencja = new Intent(this, DodajWpis.class);
         startActivityForResult(intencja, 1);
     }
 
@@ -72,6 +76,13 @@ public class MainActivity extends AppCompatActivity {
             Bundle extras = data.getExtras();
             Animal nowy = (Animal) extras.getSerializable("nowy");
             this.db.dodaj(nowy);
+            adapter.changeCursor(db.lista());
+            adapter.notifyDataSetChanged();
+        }
+        else if (requestCode == 2 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Animal nowy = (Animal) extras.getSerializable("nowy");
+            this.db.aktualizuj(nowy);
             adapter.changeCursor(db.lista());
             adapter.notifyDataSetChanged();
         }
--- app/src/main/java/com/example/android_lab/dodajWpis.java
+++ app/src/main/java/com/example/android_lab/dodajWpis.java
@@ -8,7 +8,7 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
-public class dodajWpis extends AppCompatActivity {
+public class DodajWpis extends AppCompatActivity {
     private int modyfi_id;
 
     @Override
@@ -22,8 +22,8 @@ public class dodajWpis extends AppCompatActivity {
 
         Bundle extras = getIntent().getExtras();
         try {
-            if (extras.getSerializable(„element”) != null) {
-                Animal zwierz = (Animal) extras.getSerializable(„element”);
+            if (extras.getSerializable("element") != null) {
+                Animal zwierz = (Animal) extras.getSerializable("element");
                 EditText kolor = (EditText) findViewById(R.id.kolor);
                 EditText wielkość = (EditText) findViewById(R.id.wielkosc);
                 EditText opis = (EditText) findViewById(R.id.opis);
@@ -37,14 +37,26 @@ public class dodajWpis extends AppCompatActivity {
         }
     }
 
-    public void wyslij(View view) {
-        EditText kolor = (EditText) findViewById(R.id.kolor);
-        EditText wielkosc = (EditText) findViewById(R.id.wielkosc);
-        EditText opis = (EditText) findViewById(R.id.opis);
-        Spinner gatunek = (Spinner) findViewById(R.id.gatunek);
-        Animal zwierze = new Animal(gatunek.getSelectedItem().toString(), kolor.getText().toString(), Float.valueOf(wielkosc.getText().toString()), opis.getText().toString());
+    public void wyslij (View view)
+    {
+        EditText kolor = (EditText)findViewById(R.id.kolor);
+        EditText wielkosc = (EditText)findViewById(R.id.wielkosc);
+        EditText opis = (EditText)findViewById(R.id.opis);
+        Spinner gatunek = (Spinner)findViewById(R.id.gatunek);
+
+        Animal zwierze = new Animal(
+                gatunek.getSelectedItem().toString(),
+                kolor.getText().toString(),
+                Float.valueOf(wielkosc.getText().toString()),
+                opis.getText().toString()
+        );
+
+
+        zwierze.setId(this.modyfi_id);
+
         Intent intencja = new Intent();
         intencja.putExtra("nowy", zwierze);
         setResult(RESULT_OK, intencja);
         finish();
     }
+}
